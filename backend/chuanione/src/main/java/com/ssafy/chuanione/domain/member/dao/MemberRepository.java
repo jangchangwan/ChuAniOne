3080810a61c274df1c6d0a6d19a876a5cba175e6
안세영 <tpdud285@naver.com>
2022-09-05 15:39:23 +0900

    [UPDATE] 백엔드 폴더구조 수정

---
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/domain/member/dao/MemberRepository.java
@@ -0,0 +1,2 @@
+package com.ssafy.chuanione.domain.member.dao;public interface MemberRepository {
+}
---
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/domain/member/dto/MemberRequestDto.java
@@ -0,0 +1,2 @@
+package com.ssafy.chuanione.domain.member.dto;public class MemberRequestDto {
+}
