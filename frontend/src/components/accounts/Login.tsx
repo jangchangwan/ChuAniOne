cbe1917a25e73d2dff8336e6d7ee249044d596ed
jangchangwan <tktkd6755@gmail.com>
2022-09-06 10:59:28 +0900

    Revert "Revert "[ADD] nav UI""
    This reverts commit cf600426d33850c86fe29831473f538b6fcaf812.

--- frontend/src/components/accounts/Login.tsx
+++ frontend/src/components/accounts/Login.tsx
@@ -1,5 +1,7 @@
 import './Login.css';
 import * as React from 'react';
+
+// MUI
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -10,8 +12,13 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+
+// 하위 컴포넌트
 import GoogleLogin from './GoogleLogin';
 import KakaoLogin from './KakaoLogin';
+
+// 화면전환 애니메이션
+import { motion } from 'framer-motion';
 
 const theme = createTheme();
 
@@ -26,101 +33,108 @@ function Login() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          boxShadow={2}
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundColor: 'white',
-            border: 1,
-            borderRadius: '1rem',
-            padding: 3,
-          }}
-        >
-
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <Box 
-            component="form" 
-            onSubmit={handleSubmit} 
-            noValidate sx={{ 
-              mt: 1
+    <motion.div
+      initial = {{opacity: 0}}
+      animate = {{opacity: 1}}
+      exit = {{opacity:0}}
+      transition = {{ duration: 0.5}}
+    >
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            boxShadow={2}
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              backgroundColor: 'white',
+              border: 1,
+              borderRadius: '1rem',
+              padding: 3,
             }}
-
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="이메일을 입력해주세요."
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="비밀번호를 입력해주세요."
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
 
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <Box 
+              component="form" 
+              onSubmit={handleSubmit} 
+              noValidate sx={{ 
+                mt: 1
+              }}
+
             >
-              로그인
-            </Button>
-            <Button
-              fullWidth
-              href='/signup'
-              variant="contained"
-              color="error"
-              sx={{ mt: 1, mb: 3 }}
-            >
-              회원가입
-            </Button>
-            <Grid 
-              container
-              justifyContent="center"
-              sx={{mt:2, mb:2}}
-            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="이메일을 입력해주세요."
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="비밀번호를 입력해주세요."
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                로그인
+              </Button>
+              <Button
+                fullWidth
+                href='/signup'
+                variant="contained"
+                color="error"
+                sx={{ mt: 1, mb: 3 }}
+              >
+                회원가입
+              </Button>
               <Grid 
-                item xs
-                >
-                <Link 
-                  href="/pwd" 
-                  variant="body2">
-                  비밀번호 찾기
-                </Link>
+                container
+                justifyContent="center"
+                sx={{mt:2, mb:2}}
+              >
+                <Grid 
+                  item xs
+                  >
+                  <Link 
+                    href="/pwd" 
+                    variant="body2">
+                    비밀번호 찾기
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid container>
-              <Grid item xs={4}></Grid>
-              <Grid item xs={3}>
-                <GoogleLogin></GoogleLogin>
+              <Grid container>
+                <Grid item xs={4}></Grid>
+                <Grid item xs={3}>
+                  <GoogleLogin></GoogleLogin>
+                </Grid>
+                <Grid item xs={3}>
+                  <KakaoLogin></KakaoLogin>
+                </Grid>
               </Grid>
-              <Grid item xs={3}>
-                <KakaoLogin></KakaoLogin>
-              </Grid>
-            </Grid>
 
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+        </Container>
+      </ThemeProvider>
+    </motion.div>
   );
 }
 
