aee938145aa83597645436b914cda6feb926dbfe
jangchangwan <tktkd6755@gmail.com>
2022-09-06 11:06:58 +0900

    [ADD] nav UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import Nav from './components/Nav'
-import Main from './components/Main'
-import Intro from './components/Intro'
-import PasswordFind from './components/accounts/PasswordFind'
-import Search from './components/ani/AniSearch'
-import Login from './components/accounts/Login'
-import Signup from './components/accounts/Signup'
-import OpenChat from './components/openchat/OpenChat'
-import BigVoca from './components/bigvoca/Bigvoca'
-import Mypage from './components/mypage/MyPage'
 
+import AnimatedRoutes from './components/AnimatedRoutes';
 
 function App() {
+  
   return (
     <BrowserRouter>
-      
       <Nav/>
-      <Routes>
-        {/* 여기에 링크 추가 */}
-        <Route path="/" element={<Main/>} />
-        <Route path="/intro" element={<Intro/>} />
-        <Route path="/pwd" element={<PasswordFind/>} />
-        <Route path="/search" element={<Search/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/openchat" element={<OpenChat/>} />
-        <Route path="/bigvoca" element={<BigVoca/>} />
-        <Route path="/mypage" element={<Mypage/>} />
-      </Routes>
+      <AnimatedRoutes/>
     </BrowserRouter>
   );
 }
--- frontend/src/components/Intro.tsx
+++ frontend/src/components/Intro.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Intro.css';
-
+import { motion } from 'framer-motion';
 function Intro() {
   return (
-    <div>
+    <motion.div
+      initial = {{opacity: 0}}
+      animate = {{opacity: 1}}
+      exit = {{opacity:0}}
+    >
       <h1>Intro</h1>
-    </div>
+    </motion.div>
+
   );
 }
 
