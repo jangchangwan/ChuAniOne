263e06e7bad998131299c87562aca5c19a31a398
이승현 <window8397@naver.com>
2022-09-06 21:31:52 +0900

    [ADD] AniInfo UI

--- frontend/src/components/ani/Info.tsx
+++ frontend/src/components/ani/Info.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
 
-function Info() {
+
+const ContentBox = styled.div`
+  width: 90%;
+  height: 95%;
+  padding: 2.5% 5%;
+`
+
+
+const Title = styled.h2`
+`
+
+const Content = styled.span`
+`
+
+const ContentBold = styled.span`
+  font-weight: bold;
+  margin-right: 0.5rem;
+`
+
+const Genres = styled.div`
+  margin: 1rem 0;
+`
+
+const Genre = styled.span`
+  color: white;
+  padding: 0.5rem;
+  border-radius: 1rem;
+  background-color: #f37b83;
+  margin-right: 0.5rem;
+`
+
+function Info({ recommend, }: any): any {
   return (
     <div>
-      <h1>Info</h1>
+      <ContentBox>
+        {/* <Content>{ recommend.content_rating }</Content> */}
+        <ContentBold>제작</ContentBold>
+        <Content>{recommend.production}</Content>
+        <Genres>
+          { recommend.genres.map((genre: string, idx: number) => (
+            <Genre>{genre}</Genre>
+          ))}
+        </Genres>
+        <Title>줄거리</Title>
+        <Content>{recommend.content}</Content>
+      </ContentBox>
     </div>
   );
 }
