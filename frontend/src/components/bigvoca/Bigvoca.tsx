81b8a1b688c69f00b3d2c82e31faa9811a4322e8
DESKTOP-KVCQHCD\SSAFY <tktkd6755@gmail.com>
2022-09-07 11:28:19 +0900

    [UPDATE] bigvoca UI

--- frontend/src/components/bigvoca/Bigvoca.tsx
+++ frontend/src/components/bigvoca/Bigvoca.tsx
@@ -11,10 +11,34 @@ import { motion } from 'framer-motion';
 // 하위 컴포넌트
 import WordList from './WordList';
 
+
+const PageChangeUpDiv = styled.div`
+  position: fixed;
+  z-index: 999;
+  top: -60rem;
+  left: -10rem;
+  height: 300vh;
+  width: 100vh;
+  background-color: #f37b83;
+  transform: rotate(65deg);
+`
+
+const PageChangeDownDiv = styled.div`
+  position: fixed;
+  z-index: 999;
+  top: 60rem;
+  right: -10rem;
+  height: 300vh;
+  width: 100vh;
+  background-color: #f07b4d;
+  transform: rotate(65deg);
+`
+
 const MajorTopic = styled.div`
   font-size: 3rem;
   font-weight: 700;
 `
+
 const SubTopic = styled.div`
   font-size: 1.5rem;
   font-weight: 500;
@@ -22,6 +46,7 @@ const SubTopic = styled.div`
 
 
 function Bigvoca() {
+ 
   return (
 
     <Container maxWidth="xl"
@@ -30,9 +55,27 @@ function Bigvoca() {
       }}
       >
       <motion.div
+        initial = {{ x : -1000 , y: 500 }}
+        animate = {{ x : [-1000, 550, -1000],  y: [500, -300, -500]}}
+        exit = {{ display: 'none' }}
+        
+        transition = {{ duration: '1.5'}}
+      >
+        <PageChangeUpDiv id='pagemove'></PageChangeUpDiv>
+      </motion.div>
+      <motion.div
+        initial = {{ x : 1500 , y:  -2300 }}
+        animate = {{ x : [ 1500, -300, 1500, 1500, -1500],  y: [-2300, -1500, -500, -3000,-3000]}}
+        exit = {{ display: 'none' }}
+        transition = {{ duration: '3'}}
+      >
+        <PageChangeDownDiv id='pagemove'></PageChangeDownDiv>
+      </motion.div>
+
+      <motion.div
         initial = {{ y:-400 }}
         animate = {{ y: -10 }}
-        transition = {{ delay: 0.2, type: 'spring', stiffness: 120 }}
+        transition = {{ delay: 1.2, type: 'spring', stiffness: 120 }}
       >
         <MajorTopic>
           <p>애니 자막없이 읽을 수 있는 완벽한 단어장</p>
@@ -41,7 +84,7 @@ function Bigvoca() {
       <motion.div
         initial = {{ y:-400 }}
         animate = {{ y: -10 }}
-        transition = {{ delay: 0.4, type: 'spring', stiffness: 120  }}
+        transition = {{ delay: 1.4, type: 'spring', stiffness: 120  }}
       >
         <SubTopic>
           <p>30만개의 리뷰와 2355개의 애니 분석을 통해</p>
@@ -51,7 +94,7 @@ function Bigvoca() {
       <motion.div
         initial = {{ y:-700 }}
         animate = {{ y: -10 }}
-        transition = {{ delay: 0.6, type: 'spring', stiffness: 120  }}
+        transition = {{ delay: 1.6, type: 'spring', stiffness: 120  }}
       >
         <Grid container>
           <Grid item xs={5}>
