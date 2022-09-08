f9d52a28b83af15f016a3c621e4732bc4ec592ce
금동운 <dcloud0820@gmail.com>
2022-09-09 06:03:03 +0900

    [UPDATE] MyPage 전체 틀 수정

--- frontend/src/components/mypage/MyPage.tsx
+++ frontend/src/components/mypage/MyPage.tsx
@@ -1,30 +1,47 @@
 import React from 'react'
 import styled from "styled-components"
 import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
 import MyLeft from './MyLeft';
 import MyRight from './MyRight';
 
-const MyLeftContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  margin-left: 3rem;
-`
+// const MyContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   margin: 3rem;
+// `
 
 function MyPage() {
   return (
     <div>
       <h1>MyPage</h1>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          <MyLeftContainer>
+      <Grid container>
+        <Grid item md={1}></Grid>
+        <Grid item xs={12} md={3.5}>
+          <Box m={2}>
             <MyLeft></MyLeft>
-          </MyLeftContainer>
+          </Box>
         </Grid>
-        <Grid item xs={12} md={8}>
-          <MyRight></MyRight>
+        <Grid item md={1}></Grid>
+        <Grid item xs={12} md={5.5}>
+          <Box m={2}>
+            <MyRight></MyRight>
+          </Box>
         </Grid>
       </Grid>
+      {/* <Grid spacing={2}>
+        <Grid item xs={12} md={4} padding={0}>
+          <MyContainer>
+            <MyLeft></MyLeft>
+          </MyContainer>
+        </Grid>
+        <Grid item xs={12} md={8}>
+        <MyContainer>
+          <MyRight></MyRight>
+        </MyContainer>
+        </Grid>
+      </Grid> */}
     </div>
   );
 }
