eda89f6f441af935fe10511bed5747859a52b174
안세영 <tpdud285@naver.com>
2022-09-13 10:48:10 +0900

    [ADD] 회원 Repo추가

---
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/domain/member/domain/Role.java
@@ -0,0 +1,5 @@
+package com.ssafy.chuanione.domain.member.domain;
+
+public enum Role {
+    ROLE_ADMIN, ROLE_USER
+}
--- backend/chuanione/src/main/java/com/ssafy/chuanione/global/config/SecurityConfig.java
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/global/config/SecurityConfig.java
@@ -1,4 +1,8 @@
 package com.ssafy.chuanione.global.config;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
 public class SecurityConfig {
+
 }
