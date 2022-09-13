b4510643659a7f025c3e45343bfd555e0b9c83ca
jangchangwan <tktkd6755@gmail.com>
2022-09-13 15:30:29 +0900

    [ADD] bigvoca api

--- frontend/src/request/bigvoca.ts
+++ frontend/src/request/bigvoca.ts
@@ -1,5 +1,43 @@
 import http from '../api/axios'
 
 export const getList = () => {
-  return http.get("voca/");
+  http.get('voca')
+    .then((response) =>{
+      if (response.status === 200) return response.data
+      else {
+        console.log(response);
+        alert('단어 리스트 조회 실패')
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 };
+
+export const insertMyVoca = ( id:Number ) => {
+  http.post(`voca/${id}`)
+  .then((response) =>{
+    if (response.status === 201) return response.data
+    else {
+      console.log(response);
+      alert('단어 추가 실패')
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}
+
+export const deleteMyVoca = ( id:Number ) => {
+  http.delete(`voca/${id}`)
+  .then((response) =>{
+    if (response.status === 200) return response.data
+    else {
+      console.log(response);
+      alert('단어 삭제 실패')
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}
