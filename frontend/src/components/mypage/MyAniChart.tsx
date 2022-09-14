0d822b755dae2c42ad634353996a31e134410038
jangchangwan <tktkd6755@gmail.com>
2022-09-14 12:15:41 +0900

    [ADD] font style

---
+++ frontend/src/components/mypage/MyAniChart.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Radar } from "react-chartjs-2";
+
+export class MyAniChart extends React.Component {
+
+  render() {
+    return (
+      <div>아아</div>
+    );
+  }
+}
