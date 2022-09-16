db199872f50a6fd7f5a92e2bc4c98636580a1591
ParkYuJu <qkrdbwn98@naver.com>
2022-09-16 10:36:56 +0900

    [UPDATE] sql

--- backend/chuanione/src/main/resources/data.sql
+++ backend/chuanione/src/main/resources/data.sql
@@ -5,6 +5,10 @@ select * from member;
 -- INSERT INTO MEMBER ( birthday, email, gender, introduction, nickname, password, profile, role) VALUES ('11.11','1','MAIL','자기소개','닉넴','1','1','ROLE_USER');
 -- INSERT INTO MEMBER ( birthday, email, gender, introduction, nickname, password, profile, role) VALUES ('1.1','1','FEMAIL','자기소개2','저메추','1','1','ROLE_USER');
 -- INSERT INTO MEMBER ( birthday, email, gender, introduction, nickname, password, profile, role) VALUES ('1.11','1','MAIL','자기소개3','점메추','1','1','ROLE_USER');
+INSERT INTO MEMBER ( birthday, email, gender, introduction, nickname, password, profile, role,verified)
+VALUES ('1999-03-02','window8397@naver.com','MAIL','intro','nick','$2a$10$5X4Ui5RIPP8QPUXZLC38e.W3ciUmN7jKHVRA24BVczWunJo6tp7JO','1','ROLE_USER',true);
+-- INSERT INTO MEMBER ( birthday, email, gender, introduction, nickname, password, profile, role)
+-- VALUES ('1999-03-02','window8397@naver.com','MAIL','intro','nick','$2a$10$5X4Ui5RIPP8QPUXZLC38e.W3ciUmN7jKHVRA24BVczWunJo6tp7JO','1','ROLE_USER');
 
 
 -- room
