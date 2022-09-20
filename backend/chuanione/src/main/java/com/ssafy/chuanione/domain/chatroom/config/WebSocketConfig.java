862763ce266de7377e327323ab8afa804d784da4
ParkYuJu <qkrdbwn98@naver.com>
2022-09-20 17:36:39 +0900

    [UPDATE] config

--- backend/chuanione/src/main/java/com/ssafy/chuanione/domain/chatroom/config/WebSocketConfig.java
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/domain/chatroom/config/WebSocketConfig.java
@@ -27,7 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp/chat")
+        registry.addEndpoint("/api/v1/stomp/chat.do")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
