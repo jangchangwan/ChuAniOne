cb1b9d170b98ae8ee78cc60ce2893132b010dd06
안세영 <tpdud285@naver.com>
2022-09-20 11:59:16 +0900

    [REPLACE] 메일인증 안됐을 때 400리턴

--- backend/chuanione/src/main/java/com/ssafy/chuanione/domain/member/api/MemberController.java
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/domain/member/api/MemberController.java
@@ -40,7 +40,9 @@ public class MemberController {
     @GetMapping("/email-confirm.do")
     @ApiOperation(value = "메일 인증 확인")
     public ResponseEntity<Boolean> confirmEmail(@RequestParam String token){
-        return new ResponseEntity<>(emailService.confirmEmail(token), HttpStatus.OK);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("localhost:3000/email-confirm"));
+        return new ResponseEntity<>(emailService.confirmEmail(token), HttpStatus.MOVED_PERMANENTLY);
     }
 
     @GetMapping("/email-send.do/{email}")
@@ -59,13 +61,12 @@ public class MemberController {
             throw new InvalidParameterException(result);
         }
         HttpHeaders headers = new HttpHeaders();
-        //아이디 비번 틀렸을 때 예외처리
+        //아이디 비번 틀렸을 때 예외던짐
         TokenDto tokenDto = memberService.doLogin(requestDto);
 
         //메일 인증이 안됐다면 다른곳으로 보낸다.
         if(!memberService.emailConfirmCheck(requestDto.getEmail())){
-
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
         headers.add("Auth", tokenDto.getAccessToken());
