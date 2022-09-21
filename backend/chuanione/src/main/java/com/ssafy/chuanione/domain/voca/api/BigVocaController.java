9da5bddeaa75b67427287b4b19acfbd01fc28aa1
soaeng <sodud0695@naver.com>
2022-09-21 23:36:10 +0900

    [UPDATE] test용 API 추가
    Former-commit-id: 3642dfa29e73954ab7a3765328a749696e498960

--- backend/chuanione/src/main/java/com/ssafy/chuanione/domain/voca/api/BigVocaController.java
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/domain/voca/api/BigVocaController.java
@@ -23,7 +23,15 @@ public class BigVocaController {
 
     @GetMapping("")
     @ApiOperation(value = "단어 전체 목록")
-    public ResponseEntity<List<BigVocaResponseDto>> getList(@PageableDefault(size = 8) Pageable pageable) throws Exception {
+    public ResponseEntity<List<BigVocaResponseDto>> getList(@PageableDefault(size = 8) Pageable pageable) {
+        // 한 페이지에 8개씩, 첫 페이지의 인덱스: 0
+        return new ResponseEntity<>(vocaService.getList(pageable), HttpStatus.OK);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////// Test용 (회원 인증 필요 없게)
+    @GetMapping("/list.do")
+    @ApiOperation(value = "단어 전체 목록 테스트")
+    public ResponseEntity<List<BigVocaResponseDto>> getListTest(@PageableDefault(size = 8) Pageable pageable) {
         // 한 페이지에 8개씩, 첫 페이지의 인덱스: 0
         return new ResponseEntity<>(vocaService.getList(pageable), HttpStatus.OK);
     }
