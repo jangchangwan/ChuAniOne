f8ab04809dd111461921748d12f48f81d3a31dc4
이소영 <sodud0695@naver.com>
2022-09-21 15:18:18 +0900

    [ADD] Set BigVoca Repository
    Former-commit-id: b500201a349d1b913aa5998576cd7dbd6fb05159

--- backend/chuanione/src/main/java/com/ssafy/chuanione/domain/voca/dao/BigVocaRepository.java
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/domain/voca/dao/BigVocaRepository.java
@@ -1,4 +1,14 @@
 package com.ssafy.chuanione.domain.voca.dao;
 
-public class BigVocaRepository {
+import com.ssafy.chuanione.domain.voca.domain.BigVoca;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+@Repository
+@Transactional
+public interface BigVocaRepository extends JpaRepository<BigVoca, Integer> {
+    Page<BigVoca> findAll(Pageable pageable);
 }
