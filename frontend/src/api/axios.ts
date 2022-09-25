fe91114630ff132bb4b66f75779dabcf82472d1b
안세영 <tpdud285@naver.com>
2022-09-25 20:04:34 +0900

    [REPLACE] axios url 원상태, package.json proxy 삭제
    Former-commit-id: 72e1694b2b4ccba51a6df9becb7f2cf12663e759

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://spring-boot/api/v1/",
+  baseURL: "http://j7e104.p.ssafy.io/api/v1/",
   headers: {
     "Content-type" : "application/json",
   },
