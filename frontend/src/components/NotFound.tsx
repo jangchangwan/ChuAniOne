29bfe3d91fd222f07abb8b163a3d2df45cd98a2f
DESKTOP-KVCQHCD\SSAFY <tktkd6755@gmail.com>
2022-09-26 12:06:26 +0900

    [ADD] error page
    Former-commit-id: c6785f7e0921ad164af88b539ea707bac4f9f6f3

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import Grid from "@mui/material/Grid"
+import Button from "@mui/material/Button"
+import NotFoundImg from "../assets/images/notfound.jpg"
 
 function NotFound() {
+
     return (
-        <div>404</div>
+            <Grid container
+                sx={{
+                    alignItems: 'center',
+                    height: '100vh'
+                }}
+            >
+                <Grid item xs={6}
+                    sx={{
+                        position: 'relative',
+                        left: '10%'
+                    }}
+                >
+                    <img src={NotFoundImg} alt="notfound" />
+                </Grid>
+                <Grid item xs={6}>
+                    <h1>이런 이미 제가 <span style={{color: '#87A2FB'}}>훔쳐간 페이지</span> 군요.</h1>
+                    <p>잘못된 주소이거나 권한이 없는 페이지입니다.</p>
+                    <p>츄애니원 홈으로 이동해주세요.</p>
+                    <Button variant="contained" href='/'>홈으로 이동</Button>
+                </Grid>
+            </Grid>
     )
 }
 
