563b3d8ae22fd12d58fc16e27e846eab1c15e7ae
DESKTOP-KVCQHCD\SSAFY <tktkd6755@gmail.com>
2022-09-26 11:47:18 +0900

    [ADD] 로그인 & 비로그인시 특정 url 접속권한부여
    Former-commit-id: 2b2b43345443e7a70366c00c57716b2b96050fa5

--- frontend/src/components/AnimatedRoutes.tsx
+++ frontend/src/components/AnimatedRoutes.tsx
@@ -1,7 +1,7 @@
 // 화면 전환 시 애니메이션 효과
 import { Route, Routes, useLocation } from 'react-router-dom';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Main from './Main'
 import Intro from './Intro'
@@ -16,11 +16,38 @@ import EmailCertification from './accounts/EmailCertification'
 import EmailVerificationCompleted from './accounts/EmailVerificationCompleted'
 import NotFound from './NotFound'
 
+import { useSelector } from 'react-redux'
+import initialState from '../store/Loginslice'
+import { useNavigate } from 'react-router-dom'
+
 // 애니메이션 라이브러리
 import { AnimatePresence } from 'framer-motion';
 function AnimatedRoutes() {
-
+  const navigate = useNavigate()
+  const logincheck = useSelector((state: initialState) => state.login.isLogin)
   const location = useLocation();
+
+  // 비로그인시 특정 페이지 막기
+  useEffect(():any => {
+    const path = window.location.pathname
+    if (logincheck === false) {
+      if (path === '/openchat' || path === '/bigvoca' || path === '/mypage'){
+        navigate('/login')
+      }
+    }
+  }, [window.location.pathname]);
+
+
+  // 로그인시 특정페이지 막기
+  useEffect(() => {
+    const path = window.location.pathname
+    if (logincheck === true) {
+      if (path === '/pwd' || path === '/login' || path === '/signup' || path === '/emailCertification'){
+        navigate('/')
+      }
+    }
+  }, [window.location.pathname]);
+
   return (
     <AnimatePresence>
       <Routes location={location} key={location.pathname}>
