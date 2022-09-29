61735cae8b74b503026b2c3a1590714760fc2225
박유주 <qkrdbwn98@naver.com>
2022-09-29 10:27:48 +0900

    [UPDATE] 스웨거 설명
    Former-commit-id: baed1ba7f0cb192bf29c619fe58d86a91f16849d

--- backend/chuanione/src/main/java/com/ssafy/chuanione/domain/talktalk/api/TalktalkController.java
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/domain/talktalk/api/TalktalkController.java
@@ -20,19 +20,19 @@ public class TalktalkController {
 
     @GetMapping("/list.do/{id}")
     @ApiOperation(value = "톡톡 전체 조회 (id:애니메이션)")
-    public ResponseEntity<List<TalktalkResponseDto>> getList(@PathVariable int id) {
+    public ResponseEntity<List<TalktalkResponseDto>> getList(@PathVariable(name="애니메이션 id", required=true) int id) {
         return new ResponseEntity<>(talktalkService.getList(id),HttpStatus.OK);
     }
 
     @PostMapping("/insert/{id}")
     @ApiOperation(value = "톡톡 작성 (id:애니메이션)")
-    public ResponseEntity<TalktalkResponseDto> insertTalk(@RequestBody TalktalkRequestDto dto, @PathVariable int id) {
+    public ResponseEntity<TalktalkResponseDto> insertTalk(@RequestBody TalktalkRequestDto dto, @PathVariable(name="애니메이션 id", required=true) int id) {
         return new ResponseEntity<>(talktalkService.insertTalk(dto, id),HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}/{tid}")
     @ApiOperation(value = "톡톡 삭제 (id:애니메이션), (tid:톡톡)")
-    public ResponseEntity<Void> deleteTalk(@PathVariable int id,@PathVariable int tid) {
+    public ResponseEntity<Void> deleteTalk(@PathVariable(name="애니메이션 id", required=true) int id,@PathVariable(name="톡톡 id", required=true) int tid) {
         talktalkService.deleteTalk(id, tid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
