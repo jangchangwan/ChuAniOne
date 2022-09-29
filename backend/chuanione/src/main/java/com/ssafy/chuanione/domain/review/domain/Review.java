eb85ae328186a8c4b2105a46f9b43da5a81af46c
박유주 <qkrdbwn98@naver.com>
2022-09-29 14:13:38 +0900

    [UPDATE] review entity
    Former-commit-id: 84ce5a28ca1088e3e2bca0cacbba0ee84ca7400e

--- backend/chuanione/src/main/java/com/ssafy/chuanione/domain/review/domain/Review.java
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/domain/review/domain/Review.java
@@ -1,4 +1,29 @@
 package com.ssafy.chuanione.domain.review.domain;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+@ToString
+@Table(name="review")
 public class Review {
+    @Id
+    @Column(name="review_id")
+    private Integer id;
+    private String content; // 리뷰내용
+    private LocalDateTime date; //리뷰작성일
+    private double rating; //리뷰별점
+    private Integer animation_id; //애니메이션 번호
+
+
+
 }
