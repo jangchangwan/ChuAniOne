f0d3ee0b20716220b7ebcbd2c6d3091e75727e9b
soaeng <sodud0695@naver.com>
2022-09-30 03:25:30 +0900

    [UPDATE] 애니메이션 아이디 리스트로 애니메이션 상세 조회
    Former-commit-id: ae7cf3173265101c319788ddd01b92a44ad58dc5

--- backend/chuanione/src/main/java/com/ssafy/chuanione/domain/animation/dao/AnimationRepository.java
+++ backend/chuanione/src/main/java/com/ssafy/chuanione/domain/animation/dao/AnimationRepository.java
@@ -18,8 +18,8 @@ public interface AnimationRepository extends MongoRepository<Animation, Integer>
 
     Page<Animation> findAll(Pageable pageable);
 
-     @Query(value = " { '$and' : [{ 'genres' : { '$in' : ?0 } }, { 'tags' : { '$in' : ?1 } }, { 'name' : { $regex : ?2 } }] }" )
-     Page<Animation> findSearch(String[] genres, String[] tags, String keyword, Pageable pageable);
+    @Query(value = " { '$and' : [{ 'genres' : { '$in' : ?0 } }, { 'tags' : { '$in' : ?1 } }, { 'name' : { $regex : ?2 } }] }" )
+    Page<Animation> findSearch(String[] genres, String[] tags, String keyword, Pageable pageable);
 
     @Query(value = " { '$and' : [{ 'genres' : { '$in' : ?0 } }, { 'name' : { $regex : ?1 } }] }" )
     Page<Animation> findSearchA(String[] genres,  String keyword, Pageable pageable);
@@ -28,6 +28,13 @@ public interface AnimationRepository extends MongoRepository<Animation, Integer>
     @Query(value = " { '$and' : [{ 'name' : { $regex : ?0 } }] }" )
     Page<Animation> findSearchAB( String keyword, Pageable pageable);
 
+    // 애니메이션 아이디 리스트로 해당 애니메이션 상세 조회
+    // 메인 - 8개(페이지네이션 X)
+    @Query(value = "{'id': {'$in': ?0 }}")
+    List<Animation> findAllByQuery(int[] aniId);
+    // 더보기 - 14개
+    @Query(value = "{'id': {'$in': ?0 }}")
+    Page<Animation> findAllByQuery(int[] aniId, Pageable pageable);
 
     @Query(value = "{id:?0}")
     Animation getAnimationBy_id(int id);
@@ -35,4 +42,5 @@ public interface AnimationRepository extends MongoRepository<Animation, Integer>
 
     @Query(value = "{id:?0}", fields = "{ related : 1 }")
     Animation getRelationBy_id(int id);
+
 }
