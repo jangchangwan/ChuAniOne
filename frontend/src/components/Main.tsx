9d59e1e6c9913dc14b9d9816f665c476114298c6
이승현 <window8397@naver.com>
2022-10-02 14:19:58 +0900

    [ADD] main carousel
    Former-commit-id: 90619f8e21e6699273c7f61c61d2df9cfbe166be

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Carousel from 'react-material-ui-carousel'
 import { Paper } from '@mui/material'
-import axios from "axios"
 import styled from "styled-components"
 import Box from '@mui/material/Box'
 import Modal from '@mui/material/Modal'
@@ -9,17 +8,22 @@ import AniDetail from './ani/AniDetail'
 import './Main.css'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
-import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule'
 
-import { motion } from 'framer-motion';
-import { MonetizationOn, Opacity } from '@mui/icons-material'
+
+import { motion } from 'framer-motion'
+
+// redux
+import { useDispatch } from 'react-redux'
+import store from '../store'
+import { getAniAll } from '../store/anislice'
+
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
 `
 
 const LeftContainer = styled.div`
-  background-color: yellow;
   height: 100vh;
   width: 35vw;
 `
@@ -36,10 +40,50 @@ const CarouselImg = styled.img`
 `
 
 const RightContainer = styled.div`
-  background-color: aqua;
-  height: 100vh;
-  width: 65vw;
+  /* background-color: aqua; */
+  padding: 0 3rem;
+  padding-top: 4rem;
+  height: calc(100vh - 4rem);
+  width: calc(65vw - 6rem);
+
+  overflow-y: auto;
+
+  ::-webkit-scrollbar {
+    /* background-color: ; */
+    width: 0.5rem;
+    border-radius: 0.3rem;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 0.3rem;
+    background-color: #f37b83;
+    height: 30%;
+    box-shadow: inset 0px 0px 3px white;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: #ffcdce;
+    box-shadow: inset 0px 0px 3px white;
+  }
 `
+
+const CarouselContainer = styled.div`
+  width: 100%;
+  height: 12rem;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  scroll-behavior: smooth;
+`
+
+const CarouselSection = styled.section`
+  height: 80%;
+  aspect-ratio: 4/3;
+`
+
+
+
+
 
 // const CarouselContainer = styled(Carousel)`
 //   margin-bottom: 3rem;
@@ -119,36 +163,10 @@ const RightContainer = styled.div`
 
 
 function Main() {
-  interface AniInfo {
-    air_year_quarter: string,
-    author: string[],
-    avg_rating: number,
-    cnt_short_review: number,
-    content: string,
-    content_rating: string,
-    distributed_air_time: string,
-    genres: string[],
-    highlight_video: {
-      content_id: string,
-      dash_url: string,
-      hls_url: string,
-    },
-    id: number,
-    illustrator: string[],
-    images: Array<{
-      crop_ratio: string,
-      img_url: string,
-      option_name: string,
-    }>,
-    img: string,
-    is_adult: boolean,
-    is_ending: boolean,
-    name: string,
-    production: string,
-    tags: string[],
-  }
 
-  const [recommend, setRecommend] = useState<Partial<AniInfo>>({})
+  const dispatch = useDispatch<typeof store.dispatch>()
+
+  const [data, setData] = useState<any>()
 
   const [openDetail, setOpenDetail] = useState<boolean>(false)
 
@@ -156,33 +174,16 @@ function Main() {
   const handleCloseDetail = () => setOpenDetail(false)
 
   useEffect(() => {
-    getAni()
+    loadData()
   }, [])
 
-  async function getAni(): Promise<void> {
-    const ani = await axios.get('https://laftel.net/api/items/v2/40815/')
-    await setRecommend({
-      ...recommend,
-      air_year_quarter: ani.data.air_year_quarter,
-      author: ani.data.author,
-      img: ani.data.img,
-      name: ani.data.name,
-      avg_rating: ani.data.avg_rating,
-      cnt_short_review: ani.data.cnt_short_review,
-      content: ani.data.content,
-      content_rating: ani.data.content_rating,
-      distributed_air_time: ani.data.distributed_air_time,
-      genres: ani.data.genres,
-      highlight_video: ani.data.highlight_video,
-      id: ani.data.id,
-      illustrator: ani.data.illustrator,
-      images: ani.data.images,
-      is_adult: ani.data.is_adult,
-      is_ending: ani.data.is_ending,
-      production: ani.data.production,
-      tags: ani.data.tags,
-    })
-    console.log(ani.data)
+
+  async function loadData() {
+    const res = await dispatch(getAniAll(1))
+    if (res.meta.requestStatus==="fulfilled" && res.payload) {
+      setData(res.payload.rDto)
+      console.log(res.payload)
+    }
   }
 
   const carouselImages: string[] = [
@@ -192,20 +193,6 @@ function Main() {
     'https://thumbnail.laftel.net/items/full/456559d1-6b44-4e4c-894f-e1003c4934d1.jpg',
     'https://thumbnail.laftel.net/items/full/b88d779f-f25e-4722-a6de-8a124026379a.jpg',
   ]
-
-
-  // const carouselImg: string[] = [
-  //   "https://image.laftel.net/carousel/carousel_hero_cuckooscouple_w.jpg?Expires=1662364267&Signature=KkqzyCzmMhP0a2GvoUvbDJCM8pDu-YAeWOoV7E7o1thi3LpIr391-M4gx5Pc-CD5rmTNssvDlyYU5QUwGwRDEB0fK7Hu9gvabNKn3mGLBjLtZ6tuAMQchsvKqDjhKZ0807lPS0VLq1LszLghgWUDcu08Sk2z3Mws67W6W5XxTYzxNPHU74qmZKCIiryA01ftZ8AmrudPfehRVw~zCeQAZJBgG-gSGHwbSAZPsmPXyosN3ZGsDM2joTntm5MY91FyN5vzO1u0x51QSAwEeyNton5RejKrJJ9B09rEhrSkrRx5~GAU9ZnAD5Xp1~iRyTy-9m75fVUDkOwLOtsHuYsKUw__&Key-Pair-Id=APKAJMMAKL567BYWKQ5A",
-  //   "https://image.laftel.net/carousel/carousel_hero_aria_w.jpg?Expires=1662364267&Signature=F346dJ8RbWuavoEwb3RcVvdk1GeRtLEJWXF0LA5b8Iv0e~1GuQPlN-6jjSgL442xMcIelbDYsNioT-NVLSDIm0yQxsikew30KWJ2FWB3g0yibDDhbfjLSkJG1ylSH-h39NOKc3DtNecx2JZ8~1bukvvK0PL5pZCsqhaLUVZt~uyVfOVkkOzIECVZvYF2MP~OV97RggAjJqw4FrvzXiM6qfKjaOX8dw5U5uECorlX4y2g9BOImbLjW4lzQLbljnG0mXqX0v~ZbzOW2~V4lvPaoUIU8jw0oNzcdnuyTtvkl02s1v1XXifKdGZFO32dukIJ9Tyh2GP0anQDiVRew-03~g__&Key-Pair-Id=APKAJMMAKL567BYWKQ5A",
-  //   "https://image.laftel.net/carousel/carousel_hero_gintama_finalchapter_w.jpg?Expires=1662364267&Signature=hw0ls~CWyKa2tjEfzEeWT5bKGuDSgOHBXQ-S2iRsCTeFm2BK0oFzR8TTSG3G~0RDWZaAxT0JmR2yUFuEMuEYr4j~Ck7fCAve5f~7UxTI5ugGdR85FbNwqNUTPlWvumDYQbvpO1TZ0miWIaSlr10vl23fhMP-ekUGnCuB-8OCBGRJNOMf28JuoQb6RjO3WmkVeATg0bLAdLC5QkxDqf32AHJNOKxIz11nxb07vPTq3NBgZ8RB7Ye1GubjlmqjPuXkk7NJ8s41GY5E-4PAvxjYbI5cM3qUR2kwrgVmdqJ4OPdmWKYL6VNW0QqRP2MFAUoO7Fl4C--SIaza5CyWIb5rwQ__&Key-Pair-Id=APKAJMMAKL567BYWKQ5A",
-  // ]
-
-  // const carouselImgIn: string[] = [
-  //   "https://image.laftel.net/carousel/carousel_logo_cuckoocouple.png?Expires=1662364267&Signature=f3vxJPsZ-9xSIkS6CZOjgLUJV2WXLKH4DyiZ7PbHOwMnEOAuLhHoCM8om66VwKxZfbSRVu08Mt~rcGxiW1JDuXcgXg0m5BYTYhqF1~i~xwj2kmEK8kzrypZVw8HXoRGUOCjLWJ~89C5WzW4IvunqyXLU2iL5E2xPiDVcYeprIzLx43PXkHAcARchB8qblK6mpf8SkPcPQZ4ILhxOnWDbr2OI9otQOdO-uZqw71DkuUzGUOmGFvDk5EgzQ2ybDbb6z5hLAe0kjCZTc8hXHi83P-HU1DOukkpStGtLtUhLsZNgFIZhy3BRaaEMCIlqlQryTBQm9g76D4gQ1SNhqlhsdg__&Key-Pair-Id=APKAJMMAKL567BYWKQ5A",
-  //   "https://image.laftel.net/carousel/carousel_logo_aria.png?Expires=1662364267&Signature=WtnhKcnD6daueu4SMDQFGn4s2VTPhLdqTMXmqW3GJ85Eli4O0Nu4M4i6IPQ3vKrBl2tBAoWgF3S-EhkZc7PGH4s~Bngb2GKs~kPDbrPE61gFbRoBRHnEPGr2nDgdtBMt9DnvgOkBG5aq4cDqf0fm4uBVcFRpbleRK7id3PEn2lMUd2coojK-if4hgFpCSSP2GMzx2e1rGmmq4UTLltaTN5Ed703VreKsVYackLW-O9Vww0u3ci7a7l~JaQMAKOg76-ElQn9uyOJA4kT65FQZkhQuKX1U9nGPp3q4mQahJwlzgK3gkXz0TpooxHm3r9LxjBbTL6YPrQ7JVpIHIZc1iQ__&Key-Pair-Id=APKAJMMAKL567BYWKQ5A",
-  //   "https://image.laftel.net/carousel/carousel_logo_gintama_finalchapter_1.png?Expires=1662364267&Signature=DI5b1nySfW5XKmeKumfnTEBCQemXYZr5VhNAPiUngQoyKily7FZx~lv6E9DVvRS9z8mF07cUkEAvB3FYZFx7Es3PPwLg0HZv3a7ywjdpzna7~KhqAZuaeeIo5ZCgYbRJGJ~AlQQXepqRLKOIbCDFaoXzWjTJAagS2xMJ9TMV2gZVI9iLgmBl7We2LPRnBhPtcfaB7s5GiKY9awoydVa5l9QAmG7wyUCUb2vdGnSs4BNBnMUHyA1VZtF1GeO~PqtGu4uWOj1HbUp4km~Ki~lTcHI4~CxDN~8ssLQej6HMzA~0~B8-8uj-eF3Pq4Tf2CyDWSTM1kLZu0CrKr0poy5lNw__&Key-Pair-Id=APKAJMMAKL567BYWKQ5A",
-  // ]
-
 
 
   return (
@@ -384,65 +371,33 @@ function Main() {
             ))}
           </Carousel>
         </LeftContainer>
-
+        
         <RightContainer>
-          오른쪽
+          { data ? 
+            <CarouselContainer>
+              { data.map((item, idx) => {
+                <CarouselSection id={`section${idx}`}>
+                  <div>{item.name}</div>
+                </CarouselSection>
+              })}
+            </CarouselContainer>
+          : null}
         </RightContainer>
 
 
-
-        {/* <Carousel
-          navButtonsAlwaysVisible={true}
-          NextIcon={<ArrowForwardIosIcon />}
-          PrevIcon={<ArrowBackIosNewIcon />}
-          fullHeightHover={false}
-          navButtonsProps={{          // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
-            style: {
-              background: 'none',
-            }
-          }}
-          animation={"slide"}
-          interval={6000}
-          duration={1000}
-          // swipe={true}
-          IndicatorIcon={<HorizontalRuleIcon fontSize='large' />}
-          indicatorIconButtonProps={{
-            style: {
-              color: '#e9b7ba'       // 3
-            }
-          }}
-          activeIndicatorIconButtonProps={{
-            style: {
-              color: "#f37b83"
-            }
-          }}
-          indicatorContainerProps={{
-            style: {
-              // marginTop: '50px', // 5
-              textAlign: 'center' // 4
-            }
-          }}
+        {/* <Modal
+          open={openDetail}
+          onClose={handleCloseDetail}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
         >
-          {carouselImg.map((item: string, idx: number) => (
-            <CarouselPaper elevation={0}>
-              <CarouselImg src={item} />
-              <CarouselImgIn src={carouselImgIn[idx]} className="inImage" />
-            </CarouselPaper>
-          ))}
-        </Carousel> */}
+          <Box sx={styleBoxDetail}>
+            <AniDetail aniId={item.ani_id} />
+          </Box>
+        </Modal> */}
 
-        {/* {recommend ?
-          <Modal
-            open={openDetail}
-            onClose={handleCloseDetail}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={styleBoxDetail}>
-              <AniDetail recommend={recommend} />
-            </Box>
-          </Modal> : null
-        }
+
+        {/* 
 
         <RecommendContainer>
           <RecommendTitle>당신을 위한 추천 !</RecommendTitle>
