b83d23061ecdabe63e111b3f1d5fdd9701f6713b
jangchangwan <tktkd6755@gmail.com>
2022-10-03 14:28:56 +0900

    [UPDATE] reivew
    Former-commit-id: 61456281668e20ab9c2cab6065a1b3bdb8d72ee9

--- frontend/src/components/mypage/MyReviewItem.tsx
+++ frontend/src/components/mypage/MyReviewItem.tsx
@@ -3,11 +3,15 @@ import styled from 'styled-components'
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import Rating from '@mui/material/Rating'
+import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal'
 
+import AniDetail from '../ani/AniDetail'
 // redux
 import { useDispatch } from 'react-redux'
 import store from '../../store'
 import { getAni } from '../../store/anislice'
+import { isConstructorDeclaration } from 'typescript'
 
 
 const Container = styled.div`
@@ -24,6 +28,7 @@ const ReviewBox = styled.div`
   align-items: center;
   margin-bottom: 0.5rem;
   margin-right: 1rem;
+  cursor: pointer;
 `
 
 const Namediv = styled.div`
@@ -37,8 +42,21 @@ const AniName = styled.h3`
 
 const ReviewText = styled.p`
   margin: 0;
+  white-space: pre-wrap;
 `
 
+const styleBoxDetail = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '50%',
+  height: '90%',
+  bgcolor: 'background.paper',
+  borderRadius: '0.3rem',
+  border: 'none',
+  boxShadow: 24,
+}
 
 function MyPageItem(review:any) {
 
@@ -70,6 +88,21 @@ function MyPageItem(review:any) {
   }
   const dispatch = useDispatch<typeof store.dispatch>()
   const [data, setData] = useState<Data>()
+
+  // 상세페이지 연동
+  const [openDetail, setOpenDetail] = useState<boolean>(false)
+  const [detailId, setDetailId] = useState<number | null>(null)
+  
+  // 상세페이지 켜기
+  const handleOpenDetail = (aniId) => {
+    setOpenDetail(true)
+    setDetailId(aniId)
+  }
+  // 상세페이지 끄기
+  const handleCloseDetail = () => {
+    setOpenDetail(false)
+  }
+
   async function loadData() {
     const resAni = await dispatch(getAni(review.reviewData.animation))
     // console.log(resAni)
@@ -78,13 +111,13 @@ function MyPageItem(review:any) {
     }
   }
 
-  useEffect(() =>{
+  useEffect(() =>{    
     loadData()
   },[])
   return (
     <Container>
       <Namediv>
-        <ReviewBox>
+        <ReviewBox onClick={() => handleOpenDetail(review.reviewData.animation)}>
           {
             data ?
             <AniName>{data.name}</AniName>
@@ -105,6 +138,18 @@ function MyPageItem(review:any) {
       <ReviewText>
         {review.reviewData.content}
       </ReviewText>
+      { detailId ?
+        <Modal
+          open={openDetail}
+          onClose={handleCloseDetail}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={styleBoxDetail}>
+            <AniDetail aniId={detailId} />
+          </Box>
+        </Modal>
+      : null }
     </Container>
   );
 
