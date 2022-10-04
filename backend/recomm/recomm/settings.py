71191f0feab5ca1002bcf05eae6473fe971737c3
안세영 <tpdud285@naver.com>
2022-10-04 11:07:16 +0900

    [REPLACE] 호스트 수정
    Former-commit-id: 31884f30665a667bdb01ff2cc1736520567f0401

--- backend/recomm/recomm/settings.py
+++ backend/recomm/recomm/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
